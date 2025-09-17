672d7e72fff3362390191029f9fa8934cec26cc4
ODIX.com.br <contato@odix.com.br>
2025-09-17 11:17:51 -0300

    Inicio do arquivo de envio das notificacoes (cron)

---
+++ cron/send-push.php
@@ -0,0 +1,62 @@
+<?php
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Cache-Control: post-check=0, pre-check=0', FALSE);
+header('Pragma: no-cache');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+header('Access-Control-Allow-Credentials: true');
+header('Content-Type: application/json');
+header('X-Content-Type-Options: nosniff');
+
+include('../config/inc.autoload.php');
+include('../config/inc.globals.php');
+
+$return = ["success" => false, "response" => "Parâmetros inválidos."];
+
+$api = new ApiRest('https://exp.host/--/api/v2/push/send');
+$headers = $api->getHeaders();
+$token = $api->getToken($headers);
+$vetpay = $api->payload($token);
+$method = $api->getMethod();
+
+// -- ----------------------------------------------------
+$authorization = false;
+if ($method == 'GET') 
+    $authorization = true;
+elseif (isset($vetpay['role']) && $vetpay['role']=='settings' && isset($vetpay['sub']) && $vetpay['sub']=='20250901') 
+    $authorization = true; 
+// -- ----------------------------------------------------
+if ($authorization) 
+{
+    switch($method)
+    {
+        case 'GET': 
+            //busca as push cadastradas no banco e os usuarios para envio
+
+            //monta os blocos de 100 em 100 
+
+            //envia para a api do expo e obtem o resultado
+
+            //cria objeto para atualizar os dados do usuario
+            $objusu = new Stv_usuarios_online();
+            $objusu->setId($idu);
+            $objusu->setToken_expo($exp);
+            $objusu->setToken_ativo($ati);
+            $objusu->setData_atualiza($now);
+            
+            //objeto para atualizar
+            $objdao = new Stv_usuarios_onlineDAO();
+            //$resusu = $objdao->update($objusu);
+            if ($resusu) {
+                $return = ["success" => true, "response" => "ok" ];
+            }
+            else {
+                $return = ["success" => false, "response" => "Falha"];
+            }
+        break;
+    }
+}
+
+header('Content-Type: application/json');
+echo json_encode($return);
+?>
