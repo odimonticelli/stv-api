f6c5c52a268c5ae985fae2fd1fb32a85885db25e
ODIX.com.br <contato@odix.com.br>
2025-09-17 15:25:11 -0300

    ajuste no envio do push

--- cron/send-push.php
+++ cron/send-push.php
@@ -32,26 +32,49 @@ if ($authorization)
     {
         case 'GET': 
             //busca as push cadastradas no banco e os usuarios para envio
-
+            //somente as que ainda não foram enviadas, onde a data de envio seja hoje
+            $now = date('Y-m-d');
+            $sqlpus = "status = 1";
+            $daopus = new Stv_push_notificationDAO();
+            $respus = $daopus->select($sqlpus);
             //monta os blocos de 100 em 100 
+            $jsnenv = array();
+            foreach ($respus as $vetpus) {
+                $idp = $vetpus['id'];
+                $exp = $vetpus['expo_token'];
+                $cam = $vetpus['campanha'];
+                $tit = $vetpus['titulo'];
+                $txt = $vetpus['texto'];
+                $dth = $vetpus['data_hora_agendamento'];
+                $dat = datetime_date_ptbr($dth);
+                if ($dat == $now || $dat == '0000-00-00') {
+                    //envia para a api do expo e obtem o resultado
+                    $itnenv['to'] = $exp;
+                    $itnenv['title'] = $tit;
+                    $itnenv['body'] = "$cam - $txt";
+                    $itnenv['badge'] = 1;
+                    $jsnenv[] = $itnenv;
+                    $body = json_encode($jsnenv);
+                    $head = array('Content-Type: application/json','Content-Length: '.strlen($body));
+                    $resapi = $api->get($body, $head); 
+                    $vetapi = json_decode($resapi, true);
 
-            //envia para a api do expo e obtem o resultado
-
-            //cria objeto para atualizar os dados do usuario
-            $objusu = new Stv_usuarios_online();
-            $objusu->setId($idu);
-            $objusu->setToken_expo($exp);
-            $objusu->setToken_ativo($ati);
-            $objusu->setData_atualiza($now);
-            
-            //objeto para atualizar
-            $objdao = new Stv_usuarios_onlineDAO();
-            //$resusu = $objdao->update($objusu);
-            if ($resusu) {
-                $return = ["success" => true, "response" => "ok" ];
-            }
-            else {
-                $return = ["success" => false, "response" => "Falha"];
+                    //cria objeto para atualizar os dados do usuario
+                    $objusu = new Stv_usuarios_online();
+                    $objusu->setId($idu);
+                    $objusu->setToken_expo($exp);
+                    $objusu->setData_atualiza($now);
+                    
+                    //objeto para atualizar
+                    $daousu = new Stv_usuarios_onlineDAO();
+                    //$resusu = $daousu->update($objusu);
+                    if ($resusu) {
+                        $return = ["success" => true, "response" => "ok" ];
+                    }
+                    else {
+                        $return = ["success" => false, "response" => "Falha"];
+                    }
+                }
             }
         break;
     }
